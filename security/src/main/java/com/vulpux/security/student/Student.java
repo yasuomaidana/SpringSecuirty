015b59e691faeb8e56e846b8615b6338c2df4d25
yasuomaidana <yasuomaidana@gmail.com>
2022-04-15 15:45:03 -0500

    defining student

--- security/src/main/java/com/vulpux/security/student/Student.java
+++ security/src/main/java/com/vulpux/security/student/Student.java
@@ -1,2 +1,13 @@
-package com.vulpux.security.student;public class Student {
+package com.vulpux.security.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class Student {
+    private final String studentName;
+    private final Integer studentId;
 }
