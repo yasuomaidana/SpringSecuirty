137bd1360587144421e5e427a8e2efd0df9d5abb
yasuomaidana <yasuomaidana@gmail.com>
2022-04-17 17:24:27 -0500

    Defining roles
    You can see that the roles are hashsets of permissions, you can access to the name using the name method which belongs to enum methods

--- security/src/main/java/com/vulpux/security/security/ApplicationUserRole.java
+++ security/src/main/java/com/vulpux/security/security/ApplicationUserRole.java
@@ -1,2 +1,18 @@
-package com.vulpux.security.security;public enum ApplicationUserRole {
+package com.vulpux.security.security;
+
+import com.google.common.collect.Sets;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Set;
+
+import static com.vulpux.security.security.ApplicationUserPermission.*;
+
+@AllArgsConstructor @Getter
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ,COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
 }
