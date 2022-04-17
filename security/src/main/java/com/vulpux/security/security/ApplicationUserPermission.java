d77cce6bedc78d2e124785cd8a08be7801e08ff0
yasuomaidana <yasuomaidana@gmail.com>
2022-04-17 17:27:02 -0500

    defining user permissions

--- security/src/main/java/com/vulpux/security/security/ApplicationUserPermission.java
+++ security/src/main/java/com/vulpux/security/security/ApplicationUserPermission.java
@@ -1,2 +1,15 @@
-package com.vulpux.security.security;public class ApplicationUserPermission {
+package com.vulpux.security.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
+public enum ApplicationUserPermission {
+    STUDENT_READ("student:read"),
+    STUDENT_WRITE("student:write"),
+    COURSE_READ("course:read"),
+    COURSE_WRITE("course:write");
+
+    private final String permission;
+
 }
