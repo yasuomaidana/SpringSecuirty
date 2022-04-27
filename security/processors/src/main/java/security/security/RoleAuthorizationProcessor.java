9b7ccd0a1b28733c58a5c4a7d5695e44bebe823d
ymaidana <ymaidana@skytouchtechnology.com>
2022-04-27 14:04:19 -0500

    first annotation processor generator file

--- security/processors/src/main/java/security/security/RoleAuthorizationProcessor.java
+++ security/processors/src/main/java/security/security/RoleAuthorizationProcessor.java
@@ -1,9 +1,17 @@
 package security.security;
 
+import com.google.auto.service.AutoService;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.tools.JavaFileObject;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,11 +19,13 @@ import static java.util.Arrays.asList;
 import static javax.lang.model.SourceVersion.RELEASE_8;
 import static javax.tools.Diagnostic.Kind.WARNING;
 
+@SupportedAnnotationTypes("security.security.RoleAuthorization")
+@AutoService(RoleAuthorizationProcessor.class)
 public class RoleAuthorizationProcessor extends AbstractProcessor {
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        return new HashSet<>(asList("com.vulpux.security.security.RoleAuthorization"));
+        return new HashSet<>(asList("security.security.RoleAuthorization"));
     }
 
     @Override
@@ -26,6 +36,32 @@ public class RoleAuthorizationProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         processingEnv.getMessager().printMessage(WARNING,"Testing warning");
+
+        for(Element element: roundEnv.getElementsAnnotatedWith(RoleAuthorization.class)){
+            BufferedWriter bufferedWriter;
+            JavaFileObject builderClass;
+            PackageElement packageElement = (PackageElement) element.getEnclosingElement();
+            try {
+                builderClass  = processingEnv.getFiler().createSourceFile(element.getSimpleName().toString());
+                bufferedWriter = new BufferedWriter(builderClass.openWriter());
+                if(element.getKind().isClass()){
+                    bufferedWriter.append("package ");
+                    bufferedWriter.append(packageElement.getQualifiedName());
+                    bufferedWriter.append(";");
+                    bufferedWriter.newLine();
+
+                    //bufferedWriter.append("@RoleAuthorization()");
+                    bufferedWriter.newLine();
+                    bufferedWriter.append("public class StudentManagementController{ public void nothing(){}}");
+                    //for(Element enclosed: element.getEnclosedElements()){
+
+                    //}
+                }
+                bufferedWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return false;
     }
 }
