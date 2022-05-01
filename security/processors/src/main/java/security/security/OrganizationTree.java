a13757bd22a649135adbc80e13e9738ecca3b316
ymaidana <ymaidana@skytouchtechnology.com>
2022-04-30 19:00:49 -0500

    Creating tree to read roles or permission

--- security/processors/src/main/java/security/security/OrganizationTree.java
+++ security/processors/src/main/java/security/security/OrganizationTree.java
@@ -1,2 +1,54 @@
-package security.security;public class OrganizationTree {
+package security.security;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+public class OrganizationTree {
+    ArrayList<RoleOrPermission> root = new ArrayList<>();
+    String prefix;
+    OrganizationTree(String prefix){
+        this.prefix=prefix;
+    }
+    public void add(String permissions) {
+        Queue<String> permissionStack = (Queue<String>) Arrays.stream(permissions.split("_")).collect(Collectors.toList());
+        String permission = permissionStack.poll();
+        if(root.stream().anyMatch(rootElement->rootElement.name.equals(permission))){
+
+        }else {
+            root.add(new RoleOrPermission(permission));
+        }
+    }
+}
+
+class RoleOrPermission{
+    String name;
+    String joiner;
+    ArrayList<RoleOrPermission> childrenPermissions =new ArrayList<>();
+    RoleOrPermission(String name){
+        this.name = name;
+        this.joiner ="_";
+    }
+    RoleOrPermission(String name, String joiner){
+        this.name= name;
+        this.joiner = joiner;
+    }
+    public void addChildren(RoleOrPermission children){
+        this.childrenPermissions.add(children);
+    }
+    public ArrayList<String> containedNames(){
+        ArrayList<String> names = new ArrayList<>();
+        if(childrenPermissions.size()>0){
+            names.add(name+"S");
+        } else if (childrenPermissions.size()==0) {
+            names.add(name);
+        }else{
+            childrenPermissions.stream()
+                    .forEach(childrenPermission->childrenPermission
+                            .containedNames().stream()
+                            .forEach(containedName->names.add(name+joiner+containedName)));
+        }
+        return names;
+    }
 }
