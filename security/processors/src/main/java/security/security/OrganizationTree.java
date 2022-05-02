1d4c92f87138925d7db8a3e938240ab7ef41c887
ymaidana <ymaidana@skytouchtechnology.com>
2022-05-01 23:06:26 -0500

    Changing Organization implementation, now it generates path and permissions by node

--- security/processors/src/main/java/security/security/OrganizationTree.java
+++ security/processors/src/main/java/security/security/OrganizationTree.java
@@ -1,12 +1,16 @@
 package security.security;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class OrganizationTree {
-    ArrayList<RoleOrPermission> root = new ArrayList<>();
+    ArrayList<Organization> root = new ArrayList<>();
     String prefix;
     OrganizationTree(String prefix){
         this.prefix=prefix;
@@ -14,45 +18,72 @@ public class OrganizationTree {
     public void add(String permissions) {
         Queue<String> permissionStack = (Queue<String>) Arrays.stream(permissions.split("_")).collect(Collectors.toList());
         String permission = permissionStack.poll();
-        if(root.stream().anyMatch(rootElement->rootElement.name.equals(permission))){
+        if(root.stream().anyMatch(rootElement->rootElement.getName().equals(permission))){
 
         }else {
-            root.add(new RoleOrPermission(permission));
+            root.add(new Organization(permission));
         }
     }
 }
 
-class RoleOrPermission{
-    String name;
-    String joiner;
-    ArrayList<RoleOrPermission> childrenPermissions =new ArrayList<>();
-    RoleOrPermission(String name){
+class Organization {
+    @Getter
+    private String name;
+    @Getter
+    private String pathName;
+    @Getter
+    private String setOfPermission;
+    @Setter
+    static String joiner;
+    private ArrayList<Organization> childrenPermissions =new ArrayList<>();
+    Organization(String name){
         this.name = name;
-        this.joiner ="_";
-    }
-    RoleOrPermission(String name, String joiner){
-        this.name= name;
-        this.joiner = joiner;
-    }
-    public void addChildren(String childrenName){
-        this.childrenPermissions.add(new RoleOrPermission(childrenName));
-    }
-    public ArrayList<String> containedNames(){
-        ArrayList<String> names = new ArrayList<>();
-        if(childrenPermissions.size()>0){
-            names.add(name+"S");
-            names.add(name);
-            childrenPermissions.stream()
-                    .forEach(childrenPermission->childrenPermission
-                            .containedNames().stream()
-                            .forEach(containedName->names.add(name+joiner+containedName)));
-        } else {
-            names.add(name);
-        }
-        return names;
     }
 
-    public RoleOrPermission getChildren(String childrenName){
-        return  this.childrenPermissions.stream().filter(childrenPermission->childrenPermission.name==childrenName).findFirst().orElse(null);
+    public void addChildren(String childrenName){
+        this.childrenPermissions.add(new Organization(childrenName));
+    }
+
+    private void addChildren(Organization children){
+        this.childrenPermissions.add(0,children);
+    }
+
+    private void buildPermissions(){
+        if(childrenPermissions.size()>0){
+            setOfPermission ="";
+            childrenPermissions.forEach(children->children.buildPermissions());
+            childrenPermissions.stream()
+                    .forEach(children-> setOfPermission+=children.setOfPermission+",");
+        } else {
+            setOfPermission = String.format("'%s'",name);
+        }
+    }
+
+    private void prepareOrganization(String fatherPath){
+        if(childrenPermissions.size()>0){
+            String oldName = name;
+            name = oldName+"S";
+            pathName = fatherPath+name;
+            String childrenFatherPath = pathName+"_";
+            childrenPermissions.stream()
+                    .forEach(childrenPermission->childrenPermission.prepareOrganization(childrenFatherPath));
+            Organization children = new Organization(oldName);
+            children.pathName = childrenFatherPath+oldName;
+            addChildren(children);
+        } else {
+            pathName = fatherPath+name;
+        }
+    }
+
+    public void buildOrganization(){
+        prepareOrganization("");
+        buildPermissions();
+    }
+
+    public Organization getChildren(String childrenName){
+        return  this.childrenPermissions.stream()
+                .filter(childrenPermission->
+                        Objects.equals(childrenPermission.name, childrenName))
+                .findFirst().orElse(null);
     }
 }
