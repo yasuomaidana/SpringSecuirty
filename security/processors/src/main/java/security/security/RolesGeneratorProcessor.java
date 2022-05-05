a70a11ea1e112e03f4020edff4349577109e9a0c
ymaidana <ymaidana@skytouchtechnology.com>
2022-05-05 09:17:30 -0500

    first version of procesor, it still doesn't have all the processing

--- security/processors/src/main/java/security/security/RolesGeneratorProcessor.java
+++ security/processors/src/main/java/security/security/RolesGeneratorProcessor.java
@@ -1,11 +1,16 @@
 package security.security;
 
 import com.google.auto.service.AutoService;
+import lombok.SneakyThrows;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.tools.JavaFileObject;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -14,19 +19,44 @@ import java.util.stream.Collectors;
 @AutoService(Processor.class)
 public class RolesGeneratorProcessor extends AbstractProcessor {
     private Organization roleOrganization;
+    @SneakyThrows
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         roleOrganization = new Organization("R_");
         for(Element annotation: roundEnv.getElementsAnnotatedWith(RoleApplication.class)){
-            for(Element enclosed: annotation.getEnclosedElements()
-                    .stream()
-                    .filter(enclosedRaw->
-                            enclosedRaw.getKind().name().equals("ENUM_CONSTANT"))
-                    .collect(Collectors.toList())){
-                roleOrganization.add(enclosed.getSimpleName().toString());
-            }
+            PackageElement packageElement = (PackageElement) annotation.getEnclosingElement();
+            String annotationName = annotation.getSimpleName().toString();
+            prepareOrganization(annotation);
+            generateAnnotations(packageElement.toString());
+        }
+        return false;
+    }
+
+    private void prepareOrganization(Element annotation){
+        for(Element enclosed: annotation.getEnclosedElements()
+                .stream()
+                .filter(enclosedRaw->
+                        enclosedRaw.getKind().name().equals("ENUM_CONSTANT"))
+                .collect(Collectors.toList())){
+            roleOrganization.add(enclosed.getSimpleName().toString());
         }
         roleOrganization.build();
-        return false;
+    }
+
+    private void generateAnnotations(String packageName) throws IOException {
+        Department example = roleOrganization.getDepartments().get(0);
+        JavaFileObject builderClass = processingEnv.getFiler().createSourceFile(example.getName());
+        BufferedWriter writer = new BufferedWriter(builderClass.openWriter());
+        writer.append("package ");
+        writer.append(packageName);
+        writer.append(";");
+        writer.newLine();
+        writer.newLine();
+        writer.append("public @interface ");
+        writer.append(example.getName());
+        writer.append(" {");
+        writer.newLine();
+        writer.append("}");
+        writer.close();
     }
 }
