c4a9855fe79672453bf08bf026aa12ac5a0c4a07
ymaidana <ymaidana@skytouchtechnology.com>
2022-05-08 00:53:54 -0500

    Spliting classes to allow importing and to separate logic

--- security/processors/src/main/java/security/security/Department.java
+++ security/processors/src/main/java/security/security/Department.java
@@ -1,2 +1,80 @@
-package security.security;public class Department {
+package security.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Department {
+    @Getter
+    private String name;
+    @Getter
+    private String pathName;
+    @Getter
+    private String setOfPermission;
+    @Setter
+    static String joiner = "_";
+    @Getter
+    private final ArrayList<Department> childrenPermissions = new ArrayList<>();
+
+    Department(String name) {
+        this.name = name;
+    }
+
+    @Setter
+    private static String prefix = "";
+    @Getter
+    private int level;
+
+    public void addChildren(String childrenName) {
+        this.childrenPermissions.add(new Department(childrenName));
+    }
+
+    private void addChildren(Department children) {
+        this.childrenPermissions.add(0, children);
+    }
+
+    private void buildPermissions(String fatherPath, int level) {
+        this.level = level;
+        if (childrenPermissions.size() > 0) {
+            setOfPermission = "";
+            childrenPermissions.stream().findFirst().ifPresent(masterChild -> masterChild.buildPermissions(fatherPath, this.level + 1));
+            String childFatherPath = fatherPath + name.substring(0, name.length() - 1) + joiner;
+            childrenPermissions.stream().skip(1).forEach(children -> children.buildPermissions(childFatherPath, this.level + 1));
+            childrenPermissions
+                    .forEach(children -> setOfPermission += children.setOfPermission + ",");
+            setOfPermission = setOfPermission.substring(0, setOfPermission.length() - 1);
+        } else {
+            setOfPermission = String.format("'%s'", fatherPath + name);
+        }
+    }
+
+    private void prepareOrganization(String fatherPath) {
+        if (childrenPermissions.size() > 0) {
+            String oldName = name;
+            name = oldName + "S";
+            pathName = fatherPath + name;
+            String childrenFatherPath = pathName + "_";
+            childrenPermissions
+                    .forEach(childrenPermission -> childrenPermission.prepareOrganization(childrenFatherPath));
+            Department children = new Department(oldName);
+            children.pathName = childrenFatherPath + oldName;
+            addChildren(children);
+        } else {
+            pathName = fatherPath + name;
+        }
+    }
+
+    public void buildOrganization() {
+        prepareOrganization(prefix);
+        buildPermissions("", 1);
+    }
+
+    public Department getChildren(String childrenName) {
+        return this.childrenPermissions.stream()
+                .filter(childrenPermission ->
+                        Objects.equals(childrenPermission.name, childrenName))
+                .findFirst().orElse(null);
+    }
 }
--- security/processors/src/main/java/security/security/Organization.java
+++ security/processors/src/main/java/security/security/Organization.java
@@ -12,7 +12,7 @@ public class Organization {
     private final String prefix;
     @Setter
     private String nameSplitter = "_";
-    Organization(String prefix){
+    public Organization(String prefix){
         this.prefix=prefix;
     }
     public void add(String permissions) {
@@ -55,73 +55,3 @@ public class Organization {
     }
 }
 
-class Department {
-    @Getter
-    private String name;
-    @Getter
-    private String pathName;
-    @Getter
-    private String setOfPermission;
-    @Setter
-    static String joiner ="_";
-    @Getter
-    private final ArrayList<Department> childrenPermissions =new ArrayList<>();
-    Department(String name){
-        this.name = name;
-    }
-    @Setter
-    private static String prefix="";
-    @Getter
-    private int level;
-
-    public void addChildren(String childrenName){
-        this.childrenPermissions.add(new Department(childrenName));
-    }
-
-    private void addChildren(Department children){
-        this.childrenPermissions.add(0,children);
-    }
-
-    private void buildPermissions(String fatherPath,int level){
-        this.level=level;
-        if(childrenPermissions.size()>0){
-            setOfPermission ="";
-            childrenPermissions.stream().findFirst().ifPresent(masterChild->masterChild.buildPermissions(fatherPath,this.level+1));
-            String childFatherPath = fatherPath+name.substring(0,name.length()-1)+joiner;
-            childrenPermissions.stream().skip(1).forEach(children->children.buildPermissions(childFatherPath,this.level+1));
-            childrenPermissions
-                    .forEach(children-> setOfPermission+=children.setOfPermission+",");
-            setOfPermission = setOfPermission.substring(0,setOfPermission.length()-1);
-        } else {
-            setOfPermission = String.format("'%s'",fatherPath+name);
-        }
-    }
-
-    private void prepareOrganization(String fatherPath){
-        if(childrenPermissions.size()>0){
-            String oldName = name;
-            name = oldName+"S";
-            pathName = fatherPath+name;
-            String childrenFatherPath = pathName+"_";
-            childrenPermissions
-                    .forEach(childrenPermission->childrenPermission.prepareOrganization(childrenFatherPath));
-            Department children = new Department(oldName);
-            children.pathName = childrenFatherPath+oldName;
-            addChildren(children);
-        } else {
-            pathName = fatherPath+name;
-        }
-    }
-
-    public void buildOrganization(){
-        prepareOrganization(prefix);
-        buildPermissions("",1);
-    }
-
-    public Department getChildren(String childrenName){
-        return  this.childrenPermissions.stream()
-                .filter(childrenPermission->
-                        Objects.equals(childrenPermission.name, childrenName))
-                .findFirst().orElse(null);
-    }
-}
