e84bd4df36845f15ea9278954506862accb033fe
ymaidana <ymaidana@skytouchtechnology.com>
2022-05-08 00:52:52 -0500

    Refactoring processor, now it will manage all the processing for any new annotation, I removed the logic from here since there could be different processor with different coding generation

--- security/processors/src/main/java/AnnotationsProcessor.java
+++ security/processors/src/main/java/AnnotationsProcessor.java
@@ -1,78 +1,21 @@
-package security.security;
-
 import com.google.auto.service.AutoService;
+import security.security.processors.RoleOrganization;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Processor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedSourceVersion;
-import javax.lang.model.element.Element;
+import javax.annotation.processing.*;
 import javax.lang.model.element.TypeElement;
-import javax.tools.JavaFileObject;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.util.Collections;
 import java.util.Set;
 
 import static javax.lang.model.SourceVersion.RELEASE_8;
 
 
+@SupportedAnnotationTypes({"security.security.annotations.RoleApplication","security.security.annotations.PermissionApplication"})
 @SupportedSourceVersion(RELEASE_8)
 @AutoService(Processor.class)
 public class AnnotationsProcessor extends AbstractProcessor {
-    public Organization organization;
 
+    @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv){
-
+        RoleOrganization roles = new RoleOrganization(annotations,roundEnv,processingEnv);
         return true;
-    }
-
-
-    public String generateSingleAnnotation(Department department,boolean firstTime){
-        String blankSpace = String.join("", Collections.nCopies(department.getLevel()-1, "\t"));
-        String simpleAnnotation = "";
-        simpleAnnotation += blankSpace + "@Inherited\n";
-        simpleAnnotation += blankSpace + "@Retention(RetentionPolicy.RUNTIME)\n";
-        if(department.getChildrenPermissions().size()>1){
-            simpleAnnotation += blankSpace +
-                    String.format("@PreAuthorize(\"hasAnyRole(%s)\")\n", department.getSetOfPermission());
-        }else{
-            simpleAnnotation += blankSpace +
-                    String.format("@PreAuthorize(\"hasRole(%s)\")\n", department.getSetOfPermission());
-        }
-
-        simpleAnnotation += firstTime?"public ":blankSpace;
-        simpleAnnotation += "@interface "+department.getPathName()+" {\n";
-
-        StringBuilder innerAnnotation = new StringBuilder();
-        for(Department innerDepartment: department.getChildrenPermissions()){
-            innerAnnotation.append("\n");
-            innerAnnotation.append(generateSingleAnnotation(innerDepartment,false));
-        }
-        simpleAnnotation += innerAnnotation;
-        simpleAnnotation += firstTime?"\n":"";
-        simpleAnnotation += blankSpace+"}\n";
-        return  simpleAnnotation;
-    }
-    public String generateAnnotations(String packageName, Department rootDepartment){
-        String content ="";
-        content+="package ";
-        content+=packageName;
-        content+=";\n\n";
-        content+= "import org.springframework.security.access.prepost.PreAuthorize;\n" +
-                "\n" +
-                "import java.lang.annotation.Inherited;\n" +
-                "import java.lang.annotation.Retention;\n" +
-                "import java.lang.annotation.RetentionPolicy;\n";
-        content+=generateSingleAnnotation(rootDepartment,true);
-        return content;
-    }
-    public void writeAnnotations(String packageName) throws IOException {
-        for(Department department: organization.getDepartments()){
-            JavaFileObject builderClass = processingEnv.getFiler().createSourceFile(department.getPathName());
-            BufferedWriter writer = new BufferedWriter(builderClass.openWriter());
-            writer.write(generateAnnotations(packageName,department));
-            writer.close();
-        }
     }
 }
