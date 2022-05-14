6ebb61533f224d2e08d14b19f7a53bfa3b412223
ymaidana <ymaidana@skytouchtechnology.com>
2022-05-14 17:07:20 -0500

    Adding home and login routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
 import { LoginComponent } from './login/login/login.component';
 
 const routes: Routes = [
+  {path:"",component:HomeComponent},
   {path:"login",component:LoginComponent}
 ];
 
