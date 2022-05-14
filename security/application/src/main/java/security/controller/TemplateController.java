dbf07aeaf6e0b43bc63e9e4c3ada2cdbc5c907ff
ymaidana <ymaidana@skytouchtechnology.com>
2022-05-14 17:04:00 -0500

    Creating login view and controller

--- security/application/src/main/java/security/controller/TemplateController.java
+++ security/application/src/main/java/security/controller/TemplateController.java
@@ -1,2 +1,14 @@
-package security.controller;public class TemplateController {
+package security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
 }
