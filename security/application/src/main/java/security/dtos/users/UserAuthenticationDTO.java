ab0eca023dcad579ee5343ed2d8749493c4d3957
yasuomaidana <yasuomaidana@gmail.com>
2022-06-08 10:45:53 -0500

    Defining user authentication DTO

--- security/application/src/main/java/security/dtos/users/UserAuthenticationDTO.java
+++ security/application/src/main/java/security/dtos/users/UserAuthenticationDTO.java
@@ -1,2 +1,10 @@
-package security.dtos.users;public class UserAuthenticationDTO {
+package security.dtos.users;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @Data
+public class UserAuthenticationDTO {
+    private String username;
+    private String password;
 }
