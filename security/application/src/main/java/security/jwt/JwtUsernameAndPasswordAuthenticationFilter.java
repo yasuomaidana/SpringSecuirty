c0aad5fc14fa07123abd01ef12b0be6e10377257
yasuomaidana <yasuomaidana@gmail.com>
2022-06-08 10:46:42 -0500

    Defining how JWT will manage the username and password authentication filter

--- security/application/src/main/java/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ security/application/src/main/java/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,2 +1,37 @@
-package security.jwt;public class JwtUsernameAndPasswordAuthenticationFilter {
+package security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import security.dtos.users.UserAuthenticationDTO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@AllArgsConstructor
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+
+            UserAuthenticationDTO authenticationRequest = new ObjectMapper().readValue(request.getInputStream(), UserAuthenticationDTO.class);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword());
+
+            return authenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //return super.attemptAuthentication(request, response);
+    }
 }
