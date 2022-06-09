833962f2f15e6f46adee6dd3de1d6a5ff4a15322
yasuomaidana <yasuomaidana@gmail.com>
2022-06-09 18:28:18 -0500

    Changing form authentication to JWT authentication method

--- security/application/src/main/java/security/config/security/ApplicationSecurityConfig.java
+++ security/application/src/main/java/security/config/security/ApplicationSecurityConfig.java
@@ -7,10 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import security.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 
-import java.util.concurrent.TimeUnit;
-
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import static security.config.security.ApplicationUserRole.STUDENT;
 
 @Configuration
@@ -22,31 +21,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
                 .csrf()
-                .ignoringAntMatchers("/login*","/logout*")// to ignore specific paths from csrf
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringAntMatchers("/login*","/logout*")// to ignore specific paths from csrf
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
                 .authorizeHttpRequests()
                 //WhiteListing urls
                 .antMatchers("/","/index.html","/css/*","/js/*","/login*","/logout").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin().loginPage("/login")
-                    .defaultSuccessUrl("/courses",true)
-                .and()
-                .rememberMe()
-                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                    .key("secret_key")
-                .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .logoutRequestMatcher(new
-                            AntPathRequestMatcher("/logout", "POST"))
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("XSRF-TOKEN","remember-me","JSESSIONID")
-                    .logoutSuccessUrl("/login");
+                .authenticated();
     }
 
 }
