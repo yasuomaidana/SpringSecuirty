6bb88b054a19367ab05fbff226b0e25743382989
yasuomaidana <yasuomaidana@gmail.com>
2022-06-09 18:28:44 -0500

    Creating successful authentication filter, it takes the user login payload and validates it

--- security/application/src/main/java/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ security/application/src/main/java/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package security.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,9 +11,17 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import security.dtos.users.UserAuthenticationDTO;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @AllArgsConstructor
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -32,6 +42,23 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult)
+            throws IOException, ServletException {
+        String secured_secret = Arrays.stream("secured_secret".split("[a-z]")).collect(Collectors.joining(",secured_secret,"));
+
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities",authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(secured_secret.getBytes())).compact();
+
+        response.addHeader("Authorization","Bearer "+token);
     }
 }
