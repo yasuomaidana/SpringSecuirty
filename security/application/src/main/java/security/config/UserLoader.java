e060c01e9716759e5e715ea95467fd8f3172d244
yasuomaidana <yasuomaidana@gmail.com>
2022-06-16 20:54:02 -0500

    Changing UserLoader

--- security/application/src/main/java/security/config/UserLoader.java
+++ security/application/src/main/java/security/config/UserLoader.java
@@ -8,7 +8,7 @@ import security.config.security.ApplicationUserPermission;
 import security.dtos.users.CreateUserDTO;
 import security.mappers.UserMapper;
 import security.models.users.User;
-import security.services.UserDAOService;
+import security.services.user.UserDaoServiceImplementation;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,7 +20,7 @@ import static security.config.security.ApplicationUserRole.*;
 @Component
 public class UserLoader implements ApplicationListener<ApplicationReadyEvent> {
     @Autowired
-    private UserDAOService userDAOService;
+    private UserDaoServiceImplementation userDAOServiceImplementation;
     @Autowired
     private UserMapper userMapper;
     @Override
@@ -29,15 +29,14 @@ public class UserLoader implements ApplicationListener<ApplicationReadyEvent> {
         String roles;
         String permissions;
         User userMapped;
-        if(!userDAOService.getUserByUsername("tom").isPresent()){
+        if(!userDAOServiceImplementation.getUser("tom").isPresent()){
             roles = String.valueOf(ADMIN_TRAINEE.ordinal());
             newUser =
-                    CreateUserDTO.builder().username("tom").password("pass")
-                    .roles(roles).build();
+                    CreateUserDTO.builder().username("tom").password("pass").build();
             userMapped = userMapper.createUserDtoToUser(newUser);
-            userDAOService.createUser(userMapped);
+            userDAOServiceImplementation.saveUser(userMapped);
         }
-        if (!userDAOService.getUserByUsername("linda").isPresent()){
+        if (!userDAOServiceImplementation.getUser("linda").isPresent()){
             roles = String.valueOf(ADMIN.ordinal());
             permissions = Stream.of(
                     new ApplicationUserPermission[]{STUDENT_READ, STUDENT_WRITE})
@@ -45,21 +44,18 @@ public class UserLoader implements ApplicationListener<ApplicationReadyEvent> {
             newUser =
                     CreateUserDTO.builder().username("linda")
                             .password("pass")
-                            .roles(roles)
-                            .permissions(permissions)
                             .build();
             userMapped = userMapper.createUserDtoToUser(newUser);
-            userDAOService.createUser(userMapped);
+            userDAOServiceImplementation.saveUser(userMapped);
         }
-        if (!userDAOService.getUserByUsername("anna").isPresent()){
+        if (!userDAOServiceImplementation.getUser("anna").isPresent()){
             roles = String.valueOf(STUDENT.ordinal());
             newUser =
                     CreateUserDTO.builder().username("anna")
                             .password("pass")
-                            .roles(roles)
                             .build();
             userMapped = userMapper.createUserDtoToUser(newUser);
-            userDAOService.createUser(userMapped);
+            userDAOServiceImplementation.saveUser(userMapped);
         }
     }
 }
