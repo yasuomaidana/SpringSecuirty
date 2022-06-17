0b18b8f172908e162e0ac32001fc3f0ca743a671
yasuomaidana <yasuomaidana@gmail.com>
2022-06-16 20:57:41 -0500

    Creating UserDaoService interface

--- security/application/src/main/java/security/services/user/UserDaoService.java
+++ security/application/src/main/java/security/services/user/UserDaoService.java
@@ -1,2 +1,15 @@
-package security.services;public class UserDaoService {
+package security.services.user;
+
+import security.models.users.Role;
+import security.models.users.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDaoService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    Optional<User> getUser(String name);
+    List<User> getUsers();
 }
