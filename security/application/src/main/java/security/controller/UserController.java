da873de3f125ac10a50ee5489d0ad72146a0a668
yasuomaidana <yasuomaidana@gmail.com>
2022-06-16 21:35:55 -0500

    Creating get Mapping

--- security/application/src/main/java/security/controller/UserController.java
+++ security/application/src/main/java/security/controller/UserController.java
@@ -1,2 +1,25 @@
-package security.controller;public class UserController {
+package security.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import security.models.users.User;
+import security.services.user.UserDaoService;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserDaoService userDaoService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers(){
+        return ok().body(userDaoService.getUsers());
+    }
 }
