00ee29dea592da39db5d390d652de4f8395a3d6d
yasuomaidana <yasuomaidana@gmail.com>
2022-06-30 18:49:35 -0500

    Modifying showing users endpoint to show only name, password and permmissions

--- security/application/src/main/java/security/controller/UserController.java
+++ security/application/src/main/java/security/controller/UserController.java
@@ -5,11 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import security.models.users.User;
+import security.dtos.users.UserShowAuthorities;
 import security.services.user.UserDaoService;
 
 import javax.annotation.security.PermitAll;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -21,7 +23,15 @@ public class UserController {
     private final UserDaoService userDaoService;
 
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getUsers(){
-        return ok().body(userDaoService.getUsers());
+    public ResponseEntity<List<UserShowAuthorities>> getUsers(){
+        return ok().body(userDaoService.getUsers()
+                .stream().map(user->
+                        UserShowAuthorities.builder()
+                                .id(user.getId())
+                                .username(user.getUsername())
+                                .password(user.getPassword())
+                                .permissions(new HashSet<>(user.getAuthorities()))
+                                .build())
+                .collect(Collectors.toList()));
     }
 }
