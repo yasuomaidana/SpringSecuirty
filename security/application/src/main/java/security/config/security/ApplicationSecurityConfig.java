07abf3a37bf378b0636dd0dc98b149d544d3fbaf
yasuomaidana <yasuomaidana@gmail.com>
2022-06-30 19:53:01 -0500

    Adding whitelisting paths

--- security/application/src/main/java/security/config/security/ApplicationSecurityConfig.java
+++ security/application/src/main/java/security/config/security/ApplicationSecurityConfig.java
@@ -12,7 +12,6 @@ import security.jwt.JwtTokenVerifier;
 import security.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-import static security.config.security.ApplicationUserRole.STUDENT;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +22,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
-                .csrf()
-                    .ignoringAntMatchers("/login*","/logout*")// to ignore specific paths from csrf
+                .csrf()//.disable()
+                    .ignoringAntMatchers("/login*","/logout*","/api/users/**")// to ignore specific paths from csrf
                     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                 .sessionManagement()
@@ -34,7 +33,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterAfter(new JwtTokenVerifier(jwtConfig),JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeHttpRequests()
                 //WhiteListing urls
-                .antMatchers("/","/index.html","/css/*","/js/*","/login*","/logout","/api/users").permitAll()
+                .antMatchers("/","/index.html","/css/*","/js/*","/login*","/logout","/api/users/*").permitAll()
                 .anyRequest()
                 .authenticated();
     }
