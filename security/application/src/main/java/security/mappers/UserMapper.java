211819236a9da55314216a026d081e3befcf9fb0
yasuomaidana <yasuomaidana@gmail.com>
2022-06-30 19:54:03 -0500

    Cleaning mapper logic

--- security/application/src/main/java/security/mappers/UserMapper.java
+++ security/application/src/main/java/security/mappers/UserMapper.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import security.auth.UserDetailsImplementation;
 import security.dtos.users.CreateUserDTO;
+import security.dtos.users.UserShowAuthorities;
+import java.util.HashSet;
 import security.models.users.User;
 
 import java.util.HashSet;
@@ -20,26 +22,19 @@ public abstract class UserMapper {
     @Mapping(target="password", expression = "java(encodePassword(userDto))")
     public abstract User createUserDtoToUser(CreateUserDTO userDto);
 
-    @Mapping(target = "authorities", expression = "java(getAuthoritiesFromUser(user))")
+    @Mapping(target = "authorities", expression = "java(user.getAuthorities())")
     public abstract UserDetailsImplementation userToApplicationUser(User user, boolean accountNonExpired,
                                                                     boolean accountNonLocked,
                                                                     boolean credentialsNonExpired,
                                                                     boolean enabled);
 
+    @Mapping(target = "permissions", expression = "java(getAuthoritiesFromUser(user))")
+    public abstract UserShowAuthorities UserToShowUser(User user);
     public String encodePassword(CreateUserDTO userDto){
         return passwordEncoder.encode(userDto.getPassword());
     }
 
-    public Set<? extends GrantedAuthority> getAuthoritiesFromUser(User user){
-        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
-        if(user.getRoles()!=null) {
-            user.getRoles().forEach(role -> authorities.addAll(role.getGrantedAuthorities()));
-        }
-        if(user.getPermissions()!=null) {
-            user.getPermissions()
-                    .forEach(permission ->
-                            authorities.add(new SimpleGrantedAuthority(permission.getPermission())));
-        }
-        return authorities;
+    public HashSet<GrantedAuthority> getAuthoritiesFromUser(User user){
+        return new HashSet<>(user.getAuthorities());
     }
 }
