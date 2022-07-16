ec97bf5106624985576992cd4ce62f6119c5ca8b
yasuomaidana <yasuomaidana@gmail.com>
2022-07-16 18:35:58 -0500

    Adding ignore path condition and fixing authorities == null error exception

--- security/application/src/main/java/security/jwt/JwtTokenVerifier.java
+++ security/application/src/main/java/security/jwt/JwtTokenVerifier.java
@@ -1,12 +1,15 @@
 package security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.SignatureException;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,12 +19,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
-@AllArgsConstructor
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@AllArgsConstructor @Slf4j
 public class JwtTokenVerifier extends OncePerRequestFilter {
     private JwtConfig jwtConfig;
     @Override
@@ -31,7 +35,9 @@ public class JwtTokenVerifier extends OncePerRequestFilter {
                                     @NonNull
                                     FilterChain filterChain) throws ServletException, IOException {
         String authorizationHeader = request.getHeader(jwtConfig.getAuthorizationHeader());
-        if(!Strings.isNullOrEmpty(authorizationHeader) && authorizationHeader.startsWith(jwtConfig.getTokenPrefix())){
+        if(!request.getServletPath().equals("/api/login") &&
+                !Strings.isNullOrEmpty(authorizationHeader) &&
+                authorizationHeader.startsWith(jwtConfig.getTokenPrefix())){
             String token = authorizationHeader.replace(jwtConfig.getTokenPrefix(),"");
             try{
 
@@ -47,7 +53,7 @@ public class JwtTokenVerifier extends OncePerRequestFilter {
                 List<Map<String, String>> authorities =
                         (List<Map<String, String>>) body.get(jwtConfig.getAuthoritiesPrefix());
 
-                Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities.stream()
+                Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities == null ? new HashSet<>():authorities.stream()
                         .map(
                                 m -> new SimpleGrantedAuthority(m.get(jwtConfig.getSingleAuthorityPrefix())))
                         .collect(Collectors.toSet());
@@ -56,15 +62,34 @@ public class JwtTokenVerifier extends OncePerRequestFilter {
                         new UsernamePasswordAuthenticationToken(username,null,simpleGrantedAuthorities);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (ExpiredJwtException e) {
+                log.error("Token {} cannot be trusted, token Expired", token);
                 throw new RuntimeException(String.format("Token %s cannot be trusted, token Expired", token));
             } catch (UnsupportedJwtException e) {
+                log.error("Token {} cannot be trusted, token unsupported", token);
                 throw new RuntimeException(String.format("Token %s cannot be trusted, token unsupported", token));
             } catch (MalformedJwtException e) {
+                log.error("Token {} cannot be trusted. token malformed", token);
+                response.addHeader("error",e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String,String> error = new HashMap<>();
+                error.put("error_message",e.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(),error);
                 throw new RuntimeException(String.format("Token %s cannot be trusted. token malformed", token));
             } catch (SignatureException e) {
+                log.error("Token {} cannot be trusted, signature exception", token);
                 throw new RuntimeException(String.format("Token %s cannot be trusted, signature exception", token));
             } catch (IllegalArgumentException e) {
+                log.error("Token {} cannot be trusted", token);
                 throw new RuntimeException(String.format("Token %s cannot be trusted", token));
+            } catch (AuthenticationException ae){
+                log.error("Token {} cannot be authenticated", token);
+                response.addHeader("error",ae.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String,String> error = new HashMap<>();
+                error.put("error_message",ae.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(),error);
             }
         }
 
